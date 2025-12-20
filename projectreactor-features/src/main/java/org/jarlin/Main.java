4b2a742fad68226e95ce4329a524db9369b8e024
Jarlin Fonseca <j.fonseca@nelumbo.com.co>
2025-12-20 11:50:51 -0500

    Flux Publisher

--- projectreactor-features/src/main/java/org/jarlin/Main.java
+++ projectreactor-features/src/main/java/org/jarlin/Main.java
@@ -1,13 +1,14 @@
 package org.jarlin;
 
 import lombok.extern.java.Log;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log
 public class Main {
     public static void main(String[] args) {
 
-        //Publisher
+        //Publisher Mono
         Mono<String> mono = Mono.just("Hello world")
                 .doOnNext(value -> log.info("[onNext] " + value))
                 .doOnSuccess(value -> log.info("[onSuccess] " + value))
@@ -15,6 +16,19 @@ public class Main {
 
         //Consumer
         mono.subscribe(
+                data -> log.info("Receiving data: " + data),
+                err -> log.info("Error: " + err.getMessage()),
+                () -> log.info("Complete success!")
+        );
+
+        //Publisher Flux
+        Flux<String> flux = Flux.just("Java", "Reactor", "Spring", "Reactor", "R2DBC")
+                .doOnNext(value -> log.info("[onNext] " + value))
+                .doOnComplete(() -> log.info("[onComplete]: Success"))
+                .doOnError(error -> log.info("[onError] " + error.getMessage()));
+
+        //Consumer
+        flux.subscribe(
                 data -> log.info("Receiving data: " + data),
                 err -> log.info("Error: " + err.getMessage()),
                 () -> log.info("Complete success!")
