2aeb0b331d560760620d9a28184aa16d6204e59d
Jarlin Fonseca <jarlinandres5000@gmail.com>
2026-05-02 10:26:31 -0500

    Implementando Handler – Parte 1

--- EatsHub(webflux)/eats_hub_catalog/src/main/java/com/debuggeandoideas/eats_hub_catalog/handlers/RestaurantCatalogHandler.java
+++ EatsHub(webflux)/eats_hub_catalog/src/main/java/com/debuggeandoideas/eats_hub_catalog/handlers/RestaurantCatalogHandler.java
@@ -1,11 +1,15 @@
 package com.debuggeandoideas.eats_hub_catalog.handlers;
 
+import com.debuggeandoideas.eats_hub_catalog.dtos.responses.RestaurantResponse;
 import com.debuggeandoideas.eats_hub_catalog.services.definitions.RestaurantBusinessService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
@@ -14,15 +18,33 @@ public class RestaurantCatalogHandler {
     private final RestaurantBusinessService restaurantBusinessService;
 
     public Mono<ServerResponse> getAllRestaurants(ServerRequest serverRequest){
-        return null;
+        final var restaurantFlux = this.restaurantBusinessService.readAll();
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(restaurantFlux, RestaurantResponse.class)
+                .switchIfEmpty(ServerResponse.notFound().build());
     }
 
     public Mono<ServerResponse> getRestaurantByName(ServerRequest serverRequest){
-        return null;
+       final var restaurantName = serverRequest.pathVariable("name");
+       final var monoResponse = this.restaurantBusinessService.readByName(restaurantName);
+       return monoResponse
+               .flatMap(restaurantResponse -> ServerResponse.ok()
+                       .contentType(MediaType.APPLICATION_JSON)
+                       .bodyValue(restaurantResponse))
+               .switchIfEmpty(ServerResponse.notFound().build());
     }
 
     public Mono<ServerResponse> getRestaurantsByCuisineType(ServerRequest serverRequest){
-        return null;
+        final var cuisineType = serverRequest.queryParam("cuisineType").orElse(null);
+        if(Objects.isNull(cuisineType)){
+            return ServerResponse.badRequest().bodyValue("cuisineType is required");
+        }
+        final var fluxResponse = this.restaurantBusinessService.readByCuisineType(cuisineType);
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fluxResponse, RestaurantResponse.class)
+                .switchIfEmpty(ServerResponse.notFound().build());
     }
 
     public Mono<ServerResponse> getRestaurantBetweenPrice(ServerRequest serverRequest){
