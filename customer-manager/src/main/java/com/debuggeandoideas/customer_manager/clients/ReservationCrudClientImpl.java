0da89a135de4fb92b6286bdff5f72d52de7697e5
Jarlin Fonseca <jarlinandres5000@gmail.com>
2026-05-11 06:46:17 -0500

    POST

--- customer-manager/src/main/java/com/debuggeandoideas/customer_manager/clients/ReservationCrudClientImpl.java
+++ customer-manager/src/main/java/com/debuggeandoideas/customer_manager/clients/ReservationCrudClientImpl.java
@@ -4,6 +4,7 @@ import com.debuggeandoideas.customer_manager.dtos.ReservationRequest;
 import com.debuggeandoideas.customer_manager.dtos.ReservationResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -14,6 +15,8 @@ public class ReservationCrudClientImpl implements ReservationCrudClient {
 
     private final WebClient webClient;
     private static final String RESOURCE = "catalog/reservation/";
+    private static final String ERROR_MSJ_4XX = "Error while creating reservation";
+    private static final String ERROR_MSJ_5XX = "Error while calling reservation service";
 
     @Autowired
     public ReservationCrudClientImpl(WebClient.Builder builder) {
@@ -22,7 +25,17 @@ public class ReservationCrudClientImpl implements ReservationCrudClient {
 
     @Override
     public Mono<ReservationResponse> create(ReservationRequest reservationRequest) {
-        return null;
+        log.info("Creating reservation request with restaurant id: {}", reservationRequest.getRestaurantId());
+
+        return this.webClient
+                .post()
+                .uri(RESOURCE)
+                .bodyValue(reservationRequest)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> Mono.error(new IllegalArgumentException(ERROR_MSJ_4XX)))
+                .onStatus(HttpStatusCode::is5xxServerError, response -> Mono.error(new IllegalArgumentException(ERROR_MSJ_5XX)))
+                .bodyToMono(ReservationResponse.class)
+                .doOnSuccess(res -> log.info("Reservation created successfully: {}", res));
     }
 
     @Override
