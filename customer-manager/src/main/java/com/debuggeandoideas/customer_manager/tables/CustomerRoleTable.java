5a441ac1106cb058f1ec35eefc118542fdfc65fa
Jarlin Fonseca <jarlinandres5000@gmail.com>
2026-05-12 08:06:15 -0500

    Mapeo de base de datos a clases

---
+++ customer-manager/src/main/java/com/debuggeandoideas/customer_manager/tables/CustomerRoleTable.java
@@ -0,0 +1,22 @@
+package com.debuggeandoideas.customer_manager.tables;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("customer_role")
+public class CustomerRoleTable {
+
+    @Id
+    @Column(value = "customer_id" )
+    private Long customerId;
+
+    @Column(value = "role_name" )
+    private String roleName;
+}
