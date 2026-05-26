771e5a27afbf9aedbf046bc6ac6fb8a28c923414
Jarlin Fonseca <jarlinandres5000@gmail.com>
2026-05-26 07:47:28 -0500

    ReviewService

---
+++ EatsHub(webflux)/eats_hub_catalog/src/main/java/com/debuggeandoideas/eats_hub_catalog/services/definitions/ReviewService.java
@@ -0,0 +1,11 @@
+package com.debuggeandoideas.eats_hub_catalog.services.definitions;
+
+import com.debuggeandoideas.eats_hub_catalog.collections.RestaurantCollection;
+import com.debuggeandoideas.eats_hub_catalog.dtos.Review;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface ReviewService {
+    Mono<RestaurantCollection> addRestaurantReview(UUID restaurantId, Review review);
+}
--- EatsHub(webflux)/eats_hub_catalog/src/main/java/com/debuggeandoideas/eats_hub_catalog/services/impls/ReviewStreamService.java
+++ EatsHub(webflux)/eats_hub_catalog/src/main/java/com/debuggeandoideas/eats_hub_catalog/services/impls/ReviewStreamService.java
@@ -1,7 +1,7 @@
 package com.debuggeandoideas.eats_hub_catalog.services.impls;
 
-import com.debuggeandoideas.eats_hub_catalog.dtos.Review;
 import com.debuggeandoideas.eats_hub_catalog.dtos.events.ReviewEvent;
+import com.debuggeandoideas.eats_hub_catalog.services.definitions.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,6 +11,8 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RequiredArgsConstructor
 public class ReviewStreamService {
+
+    private final ReviewService reviewService;
 
     public Mono<Void> processReview(ReviewEvent review){
         return Mono.empty();
